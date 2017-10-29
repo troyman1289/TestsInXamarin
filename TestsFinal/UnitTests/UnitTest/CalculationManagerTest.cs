1e3c5239683a50229d8a6512c513584131273002
Benjamin Proft <troyman1289@gmail.com>
2017-10-29 19:33:19 +0100

    Added unit tests

---
+++ TestsFinal/UnitTests/UnitTest/CalculationManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Backend.Business;
+using Backend.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CalculationManagerTest
+    {
+
+        //SETUP???
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockDatabase = new Mock<IDataAccess>();
+            var manager = new CalculationManager(mockDatabase.Object);
+
+
+        }
+    }
+}
---
+++ TestsFinal/UnitTests/UnitTest/OperationsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Backend.Model.Operator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class OperationsTest
+    {
+        [TestMethod]
+        public void AdditionTest()
+        {
+            var result = Operators.Addition.Calculate(4, 5);
+            Assert.AreEqual(result,9);
+        }
+    }
+}
