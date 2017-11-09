150cc3d6846006ec5eccb471b25948517612777d
Benjamin Proft <troyman1289@gmail.com>
2017-11-09 22:27:34 +0100

    Started with Android Implementation

---
+++ TestsFinal/TestsFinal.DroidNative/CreateGlobalCalculationActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TestsFinal.DroidNative
+{
+    [Activity(Label = "CreateGlobalCalculationActivity")]
+    public class CreateGlobalCalculationActivity : Activity
+    {
+
+        private EditText _descriptionEditText;
+        private EditText _creatorEditText;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //SetContentView(Resource.);
+
+            _descriptionEditText = FindViewById<EditText>(Resource.Id.DescriptionEditText);
+            Bindings.Add(this.SetBinding(() => ViewModel.Description, () => _descriptionEditText.Text, BindingMode.TwoWay));
+
+            _creatorEditText = FindViewById<EditText>(Resource.Id.CreatorEditText);
+            Bindings.Add(this.SetBinding(() => ViewModel.Creator, () => _creatorEditText.Text, BindingMode.TwoWay));
+
+            var saveButton = FindViewById<Button>(Resource.Id.dialogButtonSave);
+            saveButton.SetCommand("Click", ViewModel.SaveShoppingListElementCommand);
+        }
+    }
+}
--- TestsFinal/TestsFinal.DroidNative/MainActivity.cs
+++ TestsFinal/TestsFinal.DroidNative/MainActivity.cs
@@ -1,19 +1,82 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Toolbar = Android.Widget.Toolbar;
 
 namespace TestsFinal.DroidNative
 {
-    [Activity(Label = "TestsFinal.DroidNative", MainLauncher = true)]
-    public class MainActivity : Activity
+    [Activity(Label = "TestsFinal.DroidNative", MainLauncher = true, Theme = "@style/MainTheme")]
+    public class MainActivity : AppCompatActivity
     {
+        private RelativeLayout _progressBarRelativeLayout;
+        private ListView _listView;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+            toolbar.InflateMenu(Resource.Menu.ShoppingListElementListMenu);
+            var getButton = toolbar.FindViewById<AppCompatButton>(Resource.Id.GetButton);
+            getButton.SetBackgroundColor(Color.Transparent);
+            getButton.Text = "Get";
+            getButton.SetCommand("Click", ViewModel.RetrieveShoppingListElementsCommand);
+
+            _progressBarRelativeLayout = FindViewById<RelativeLayout>(Resource.Id.ProgressBarLayout);
+            Bindings.Add(this.SetBinding(() => ViewModel.IsBusy, () => _progressBarRelativeLayout.Visibility, BindingMode.OneWay)
+                .ConvertSourceToTarget(b => b ? ViewStates.Visible : ViewStates.Invisible));
+
+            _listView = FindViewById<ListView>(Resource.Id.ShoppingListListView);
+            _listView.ItemClick += OnItemClick;
+            _listView.Adapter = ViewModel.ShoppingListElementCollection.GetAdapter(GetShoppingListElementAdapter);
+            RegisterForContextMenu(_listView);
+
+            var floatingButton = FindViewById<FloatingActionButton>(Resource.Id.AddButton);
+            floatingButton.SetCommand("Click", ViewModel.AddShoppingListElementCommand);
         }
+
+        private void OnItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var item = ViewModel.ShoppingListElementCollection[e.Position];
+            if (item == null)
+                return;
+
+            if (_currentOpenShoppingListElement != null) {
+                _currentOpenShoppingListElement.DetailElements.CollectionChanged -= OnCurrentShoppingListElementDetailElementsChanged;
+            }
+
+            _currentOpenShoppingListElement = item;
+            _currentOpenShoppingListElement.DetailElements.CollectionChanged += OnCurrentShoppingListElementDetailElementsChanged;
+
+            ViewModel.OpenShoppingListElementCommand.Execute(item);
+        }
+
+        public override void OnCreateContextMenu(IContextMenu menu, View v, IContextMenuContextMenuInfo menuInfo)
+        {
+            base.OnCreateContextMenu(menu, v, menuInfo);
+            MenuInflater.Inflate(Resource.Menu.DeleteContextMenu, menu);
+        }
+
+        public override bool OnContextItemSelected(IMenuItem item)
+        {
+            var menuInfo = (AdapterView.AdapterContextMenuInfo)item.MenuInfo;
+
+            switch (item.ItemId) {
+                case Resource.Id.DeleteItem:
+                    var element = ViewModel.ShoppingListElementCollection[menuInfo.Position];
+                    ViewModel.RemoveShoppingListElementCommand.Execute(element);
+                    return true;
+            }
+
+            return base.OnContextItemSelected(item);
+        }
+
     }
 }
 
